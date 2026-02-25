311e0cac8a422bde441340e35ac2d964be7ad74e
Bixente94 <Bixente.barnetche@gmail.com>
2026-02-25 22:31:08 +0100

    Create App.py

---
+++ App.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+debut_poules = datetime(2026,6,20)
+fin_poules = debut_poules + timedelta(days = 45)
+print(f"Début des poules le {debut_poules.strftime('%A %d %B')}.")
+
+jours_parties = []
+jour_actuel = debut_poules
+while jour_actuel <= fin_poules :
+    if jour_actuel.weekday() >= 2:
+        jours_parties.append(jour_actuel)
+    jour_actuel += timedelta(days=1)
+ 
+creneaux = [] 
+for jours in jours_parties: 
+   creneau_19 = jours + timedelta(hours = 19)
+   creneau_20 = jours + timedelta(hours = 20)
+   creneaux.extend([creneau_19, creneau_20])
+   for creneau in creneaux:   
+     print(creneau.strftime("%A %d %B à %Hh")) 
+     
+   

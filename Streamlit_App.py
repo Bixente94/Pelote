6b0dbd5cc0e65d4da98b846f1fe1ce0662286b72
Bixente94 <Bixente.barnetche@gmail.com>
2026-02-26 22:27:31 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,31 +1,40 @@
 from datetime import datetime, timedelta
 import streamlit as st
+
+# Dates tournoi
 debut_poules = datetime(2026,6,20)
 fin_poules = debut_poules + timedelta(days = 45)
-print(f"Début des poules le {debut_poules.strftime('%A %d %B')}.")
 
+# Jours et créneaux
 jours_parties = []
 jour_actuel = debut_poules
-while jour_actuel <= fin_poules :
-    if jour_actuel.weekday() >= 2:
-        jours_parties.append(jour_actuel)
-    jour_actuel += timedelta(days=1)
- 
 creneaux = [] 
-for jours in jours_parties: 
-   creneau_19 = jours + timedelta(hours = 19)
-   creneau_20 = jours + timedelta(hours = 20)
+while jour_actuel <= fin_poules:
+  if jours.weektime() >= 2:
+   creneau_19 = jours.replace(hours = 19)
+   creneau_20 = jours.replace(hours = 20)
    creneaux.extend([creneau_19, creneau_20])
+  jour_actuel += timedelta(days = 1)
 
 st.title("Inscription tournoi Halsou 2026")
 st.write("page d’inscription")
 st.metric("total de creneaux :", len(creneaux))
+st.info("cochez au moins 15 reponses")
+#Formulaire
+with st.form("Formulaire d’inscription"):
+  nom = st.text_input("Nom")
+  prenom = st.text_input("Prénom")
+  tel = st.text_input("Numéro de téléphone")
+  poule = st.selectbox("Série", [1ère série A, 1ère série B, 2ème série, 3ème série)
 
-with st.expander("montrer les créneaux"):
-  for creneau in creneaux:   
-     checkbox = st.checkbox(creneau.strftime("%A %d %B à %Hh"))
-     checkbox
-     dispos = []
-     if checkbox == True:
-       dispos.append(creneau)
-   
+  dispos = {}
+  with st.expander("Disponibilités"):
+    for creneau in creneaux:   
+      label = creneau.strftime("%A %d %B à %Hh"))
+      dispos[creneau] = st.checkbox(label, key = creneau)
+        
+  submit = st.form_submit_button("Valider")
+
+if submit :
+    disponibilites = [date for date, coche in dispos if coche)
+    st.write(disponibilites)
